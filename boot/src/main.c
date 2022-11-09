2a54dc08ee353644b1a5428b50ef749b4413857b
Ian <spurious.vector@gmail.com>
2022-11-09 05:12:59 -0500

    init

--- boot/src/main.c
+++
@@ -1,9 +0,0 @@
-#include <efi.h>
-#include <efilib.h>
-
-
-EFI_STATUS efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE* systemTable) {
-  InitializeLib(imageHandle, systemTable);
-  Print(L"YOOO!\n");
-  __asm__ __volatile__("cli; hlt");
-}
