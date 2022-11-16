9fdb814449baa22fcbe504a2ef256f1c7b0e499b
Ian <spurious.vector@gmail.com>
2022-11-15 20:52:57 -0500

    Feat: Init ports and detect drives.

--- sys/include/drivers/hdd/ahci.h
+++ sys/include/drivers/hdd/ahci.h
@@ -30,6 +30,7 @@ typedef volatile struct HBA_MEM {
   uint32_t cap;         // Host capability.
   uint32_t ghc;         // Global host control.
   uint32_t is;          // Interrupt status.
+  uint32_t pi;          // Port implemented.
   uint32_t vs;          // Version.
   uint32_t ccc_ctl;     // Command completion coalescing control
   uint32_t ccc_ports;   // Command completion coalescing ports.
@@ -42,6 +43,25 @@ typedef volatile struct HBA_MEM {
   uint8_t vendor[0x100-0xA0];
   HBA_PORT ports[1];    // 1 ~ 32.
 } HBA_MEM;
+
+
+typedef struct HBA_CMD_HEADER {
+  uint8_t cfl : 5;          // CMD FIS length in dwords.
+  uint8_t a : 1;            // ATAPI.
+  uint8_t w : 1;            // Write, 1: H2D, 0: D2H.
+  uint8_t p : 1;            // Prefetchable.
+  uint8_t r : 1;            // Reset.
+  uint8_t b : 1;            // BIST.
+  uint8_t c : 1;            // Clear busy upon R_OK.
+  uint8_t rsv0 : 1;         // Reserved.
+  uint8_t pmp : 4;          // Port multiplier port.
+
+  uint16_t prdtl;           // Physical region descriptor table length in entries.
+  volatile uint32_t prdbc;  // Physical region descriptor byte count transferred.
+  uint32_t ctba;            // Command table descriptor base address.
+  uint32_t ctbau;           // Command table descriptor base address upper 32 bits.
+  uint32_t rsv1[4];         // Reserved.
+} HBA_CMD_HEADER;
 
 
 void ahci_init(void);
--- sys/src/drivers/hdd/ahci.c
+++ sys/src/drivers/hdd/ahci.c
@@ -2,16 +2,159 @@
 #include <arch/bus/pci.h>
 #include <lib/module.h>
 #include <lib/log.h>
+#include <lib/string.h>
+#include <lib/math.h>
+#include <mm/heap.h>
+#include <mm/vmm.h>
 
 MODULE("ahci");
 
 #define SATA_CONTROLLER_PCI_CLASS 0x01
 #define SATA_CONTROLLER_PCI_SUBCLSS 0x06
 
+#define HBA_PORT_IPM_ACTIVE 1
+#define HBA_PORT_DET_PRESENT 3
+
+#define HBA_PxCMD_ST    (1 << 0)
+#define HBA_PxCMD_FRE   (1 << 4)
+#define HBA_PxCMD_FR    (1 << 14)
+#define HBA_PxCMD_CR    (1 << 15)
+
+#define	SATA_SIG_ATA 0x00000101     // SATA drive
+#define	SATA_SIG_ATAPI 0xEB140101   // SATAPI drive
+#define	SATA_SIG_SEMB 0xC33C0101    // Enclosure management bridge
+#define	SATA_SIG_PM 0x96690101      // Port multiplier.
+
+#define AHCI_DEV_NULL 0
+#define AHCI_DEV_SATA 1
+#define AHCI_DEV_SEMB 2
+#define AHCI_DEV_PM 3
+#define AHCI_DEV_SATAPI 4
+
 
 static pci_device_t dev;
 static HBA_MEM* abar = NULL;
 
+
+static void stop_cmd(HBA_PORT* port) {
+  // Clear ST to remove the ability
+  // of the HBA processing commands.
+  port->cmd &= ~(HBA_PxCMD_ST);
+
+  // Prevent received FISes from being
+  // accepted by HBA.
+  port->cmd &= ~(HBA_PxCMD_FRE);
+
+  // Wait until the FIS Receive DMA engine 
+  // and the command list DMA engine is no longer
+  // running for this port.
+  while (port->cmd & (HBA_PxCMD_FR | HBA_PxCMD_CR));
+}
+
+
+static void start_cmd(HBA_PORT* port) {
+  // Wait until command list DMA engine is no longer
+  // running for this port.
+  while (port->cmd & (HBA_PxCMD_CR));
+
+  // Allow received FISes to be accepted 
+  // by the HBA and allow the HBA
+  // to process commands.
+  port->cmd |= HBA_PxCMD_FRE;
+  port->cmd |= HBA_PxCMD_ST;
+}
+
+
+static void port_init(HBA_PORT* port) {
+  // Stop the command engine.
+  stop_cmd(port);
+  
+  // Setup the command list base.
+  void* cmdlist_base = kmalloc(0x1000 + 0x3E8);
+  kmemzero(cmdlist_base, 0x1000 + 0x3E8);
+
+  uint64_t cmdlist_phys_base = (uint64_t)cmdlist_base - VMM_HIGHER_HALF;
+  cmdlist_phys_base = ALIGN_UP(cmdlist_phys_base, 0x3E8);
+
+  port->clb = cmdlist_phys_base & 0xFFFFFFFF;
+  port->clbu = cmdlist_phys_base >> 32;
+
+  // Set FB and FBU.
+  void* fb_virt = kmalloc(0x1000 + 0x100);
+  kmemzero(fb_virt, 0x1000 + 0x100);
+  
+  uint64_t fb_phys = (uint64_t)fb_virt - VMM_HIGHER_HALF;
+  fb_phys = ALIGN_UP(fb_phys, 0x100);
+
+  port->fb = fb_phys & 0xFFFFFFFF;
+  port->fbu = fb_phys >> 32;
+
+  // Setup the command header.
+  HBA_CMD_HEADER* cmdheader = (HBA_CMD_HEADER*)cmdlist_base;
+
+  for (int i = 0; i < 32; ++i) {
+    cmdheader[i].prdtl = 8;         // 8 prdtl entries per command table.
+                                    // 256-bytes per command table.
+    
+    void* ctba = kmalloc(0x2050);
+    kmemzero(ctba, 256*32);
+
+    uint64_t phys = (uint64_t)ctba - VMM_HIGHER_HALF;
+    cmdheader[i].ctba = phys & 0xFFFFFFFF;
+    cmdheader[i].ctbau = phys >> 32;
+  }
+
+  // Start the command engine.
+  start_cmd(port);
+}
+
+
+static uint32_t check_type(HBA_PORT* p) {
+  uint32_t ssts = p->ssts;
+  uint8_t ipm = (ssts >> 8) & 0x0F;
+  uint8_t det = ssts & 0x0F;
+
+  if (det != HBA_PORT_DET_PRESENT || ipm != HBA_PORT_IPM_ACTIVE) {
+    return AHCI_DEV_NULL;
+  }
+
+  switch (p->sig) {
+    case SATA_SIG_ATAPI:
+      return AHCI_DEV_SATAPI;
+    case SATA_SIG_SEMB:
+      return AHCI_DEV_SEMB;
+    case SATA_SIG_PM:
+      return AHCI_DEV_PM;
+    default:
+      return AHCI_DEV_SATA;
+  }
+}
+
+static void find_ports(void) {
+  uint32_t pi = abar->pi;
+
+  for (uint8_t i = 0; i < 32; ++i, pi >>= 1) {
+    if (pi & 1) {
+      int dt = check_type(&abar->ports[i]);
+
+      switch (dt) {
+        case AHCI_DEV_SATA:
+          PRINTK_SERIAL("[%s]: SATA drive found @HBA_PORT_%d\n", MODULE_NAME, i);
+          port_init(&abar->ports[i]);
+          break;
+        case AHCI_DEV_SEMB:
+          PRINTK_SERIAL("[%s]: Enclosure management bridge found @HBA_PORT_%d (ignoring)\n", MODULE_NAME, i);
+          break;
+        case AHCI_DEV_SATAPI:
+          PRINTK_SERIAL("[%s]: SATAPI drive found @HBA_PORT_%d (ignoring)\n", MODULE_NAME, i);
+          break;
+        case AHCI_DEV_PM:
+          PRINTK_SERIAL("[%s]: Port multiplier found @HBA_PORT_%d (ignoring)\n", MODULE_NAME, i);
+          break;
+      }
+    }
+  }
+}
 
 
 void ahci_init(void)  {
@@ -27,4 +170,5 @@ void ahci_init(void)  {
   PRINTK_SERIAL("[%s]: Bus mastering enabled for SATA controller.\n", MODULE_NAME);
 
   abar = (HBA_MEM*)(uint64_t)dev.bar5;
+  find_ports();
 }
