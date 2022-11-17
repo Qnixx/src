e111b27e03a48b13aba0a16fd26e1e17b276101a
Ian <spurious.vector@gmail.com>
2022-11-17 01:43:49 -0500

    Fix: VMM_PHYS macro.

--- sys/include/mm/vmm.h
+++ sys/include/mm/vmm.h
@@ -3,12 +3,12 @@
 
 #include <lib/limine.h>
 #include <lib/types.h>
+#include <lib/math.h>
 
 extern volatile struct limine_hhdm_request hhdm_request;
 
 #define VMM_HIGHER_HALF (hhdm_request.response->offset)
-#define VMM_PHYS(addr) ((uintptr_t)addr - VMM_HIGHER_HALF)
-#define VMM_VIRT(addr) ((uintptr_t)addr + VMM_HIGHER_HALF)
+#define VMM_PHYS(addr) (vmm_get_phys((void*)get_cr3(), (uintptr_t)addr))
 #define PTE_ADDR_MASK 0x000FFFFFFFFFF000
 #define PTE_PRESENT (1ull << 0)
 #define PTE_WRITABLE (1ull << 1)
@@ -16,6 +16,7 @@ extern volatile struct limine_hhdm_request hhdm_request;
 #define PTE_NX (1ULL << 63)
 #define PTE_GET_ADDR(VALUE) ((VALUE) & PTE_ADDR_MASK)
 #define PAGE_SIZE 0x1000
+#define PAGE_ALIGN(addr) (ALIGN_UP((uintptr_t)addr, PAGE_SIZE))
 
 void vmm_map_page(uintptr_t* pml4, uintptr_t vaddr, uintptr_t phys, uint64_t flags);
 void vmm_unmap_page(uintptr_t* pml4, uintptr_t vaddr);
