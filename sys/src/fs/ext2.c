1d3d5d0cf273d91b04f3d57c2a534815895b7b92
Ian <spurious.vector@gmail.com>
2022-11-18 21:42:49 -0500

    Refactor: Cleanup.

--- sys/src/fs/ext2.c
+++ sys/src/fs/ext2.c
@@ -12,13 +12,9 @@ static dev_driver_t* block_driver = NULL;
 
 
 void ext2_init(void) {
-  PRINTK_SERIAL("[%s] Locating a block driver..\n", MODULE_NAME);
+  printk("[%s] Locating a block driver..\n", MODULE_NAME);
 
   block_driver = find_driver(DEV_BLOCK, 0);
   ASSERT(block_driver != NULL, "No block driver found!\n");
-  PRINTK_SERIAL("[%s]: Found block driver: %s\n", MODULE_NAME, block_driver->name);
-
-  uint16_t* buf = vmm_alloc_page();
-  disk_read_lba(block_driver, 0, 1, buf);
-  printk("%x\n", buf[0]);
+  printk("[%s]: Found block driver: %s\n", MODULE_NAME, block_driver->name);
 }
