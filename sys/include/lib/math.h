565099fb83f50de95ff167c97bd8605f638e0b59
Ian <spurious.vector@gmail.com>
2022-11-20 14:24:50 -0500

    Feat: Add OFF_BY in math.h

--- sys/include/lib/math.h
+++ sys/include/lib/math.h
@@ -5,5 +5,6 @@
 #define ALIGN_UP(address, align)    (((address) + (align)-1) & ~((align)-1))
 #define BIG_ENDIAN(v) ((((v) & 0xFF) << 8) | (((v) >> 8) & 0xFF))
 #define IS_POWER_OF_2(x) ((x & (x-1)) == 0)
+#define OFF_BY(off, by) (~(off % ~(by)))
 
 #endif
