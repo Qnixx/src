3f72533a949a9180515daa71dca3076b8f0ba910
Ian <spurious.vector@gmail.com>
2022-11-21 14:34:35 -0500

    Feat: Boot module.

---
+++ sys/src/lib/bootmodule.c
@@ -0,0 +1,21 @@
+#include <lib/bootmodule.h>
+
+#include <lib/module.h>
+#include <lib/string.h>
+#include <lib/limine.h>
+
+static volatile struct limine_module_request mod_req = {
+  .id = LIMINE_MODULE_REQUEST,
+  .revision = 0
+};
+
+
+struct limine_file* get_module(const char* path) {
+  for (uint64_t i = 0; i < mod_req.response->module_count; ++i) {
+    if (kstrcmp(mod_req.response->modules[i]->path, path) == 0) {
+      return mod_req.response->modules[i];
+    }
+  }
+
+  return NULL;
+}
