fecba69b057851de6c4596f83d7d973c14626563
Kaimakan71 <Kai71Games@outlook.com>
2022-11-21 19:15:19 -0500

    Fix: Move month names out of time.h

--- sys/include/lib/time.h
+++ sys/include/lib/time.h
@@ -1,7 +1,7 @@
 #ifndef TIME_H_
 #define TIME_H_
 
-#include <lib/math.h>
+#include <lib/types.h>
 
 #define SECONDS_PER_YEAR 31557600
 #define SECONDS_PER_MONTH 2629800
@@ -15,6 +15,6 @@
 #define GET_HOUR(stamp) ((stamp % SECONDS_PER_DAY) / SECONDS_PER_HOUR)
 #define GET_MINUTE(stamp) ((stamp % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE)
 
-const char* month_names[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+extern const char* month_names[];
 
 #endif
---
+++ sys/src/lib/time.c
@@ -0,0 +1,4 @@
+#include <lib/time.h>
+#include <lib/math.h>
+
+const char* month_names[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
