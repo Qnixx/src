2858db5a28c3de1dec07d235894b8263480ac488
Ian <spurious.vector@gmail.com>
2022-11-27 21:21:57 -0500

    Update: asm.h

--- sys/include/lib/asm.h
+++ sys/include/lib/asm.h
@@ -5,4 +5,9 @@
 #define _packed __attribute__((packed))
 #define ASMV(_asm) __asm__ __volatile__(_asm)
 
+#define CLI_SLEEP \
+  for (unsigned long long i = 0; i < 1000000; ++i) {          \
+    ASMV("cli");                                              \
+  }
+
 #endif
