0b162d6db97bf1cd56cd2c44a5682eb5326b1422
Ian <spurious.vector@gmail.com>
2022-11-29 02:05:12 -0500

    Misc: Update asm.h

--- sys/include/lib/asm.h
+++ sys/include/lib/asm.h
@@ -1,7 +1,7 @@
 #ifndef ASM_H_
 #define ASM_H_
 
-
+#define _noreturn __attribute__((noreturn))
 #define _packed __attribute__((packed))
 #define _isr __attribute__((interrupt))
 #define ASMV(_asm) __asm__ __volatile__(_asm)
