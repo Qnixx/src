b7ad6d4df05ed4657d9ebffbbfcb917d1caaf0fc
Ian <spurious.vector@gmail.com>
2022-11-29 23:46:44 -0500

    Feat: Trapframe.

--- sys/include/proc/proc.h
+++ sys/include/proc/proc.h
@@ -6,6 +6,30 @@
 
 typedef uint16_t pid_t;
 
+typedef struct {
+  uint64_t rdi;
+  uint64_t rsi;
+  uint64_t rbx;
+  uint64_t rdx;
+  uint64_t rcx;
+  uint64_t rax;
+  uint64_t r8;
+  uint64_t r9;
+  uint64_t r10;
+  uint64_t r11;
+  uint64_t r12;
+  uint64_t r13;
+  uint64_t r14;
+  uint64_t r15;
+  uint64_t rbp;
+  uint64_t trapno;
+  uint64_t rip;
+  uint16_t cs;
+  uint64_t rflags;
+  uint64_t rsp;
+  uint16_t ss;
+} _packed trapframe_t;
+
 
 typedef struct Process {
   pid_t pid;
