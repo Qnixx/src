29977f0f7380944238b67212cb39005ae324de25
Ian <spurious.vector@gmail.com>
2022-12-02 17:43:08 -0500

    asmlib: Add unused macro.

--- sys/include/lib/asm.h
+++ sys/include/lib/asm.h
@@ -8,5 +8,6 @@
 #define _noreturn __attribute__((noreturn))
 #define _naked __attribute__((naked))
 #define _aligned(n) __attribute__((aligned(n)))
+#define _unused __attribute__((unused))
 
 #endif
