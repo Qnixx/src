f1d60b36eaae1e4e739ba704075e06bc12d5be00
Ian <spurious.vector@gmail.com>
2022-12-02 03:20:25 -0500

    Feat: Add virtual memory manager

--- sys/include/mm/vmm.h
+++ sys/include/mm/vmm.h
@@ -9,5 +9,19 @@
 extern volatile struct limine_hhdm_request hhdm_request;
 
 
+typedef uintptr_t pagemap_t;
+typedef uintptr_t vaddr_t;
+typedef uintptr_t paddr_t;
+
+#define PTE_ADDR_MASK 0x000FFFFFFFFFF000
+#define PTE_PRESENT (1ull << 0)
+#define PTE_WRITABLE (1ull << 1)
+#define PTE_USER (1ULL << 2)
+#define PTE_NX (1ULL << 63)
+#define PTE_GET_ADDR(VALUE) ((VALUE) & PTE_ADDR_MASK)
+#define PAGE_SIZE 0x1000
+
+
+void vmm_map_page(pagemap_t top_level, vaddr_t vaddr, paddr_t paddr, uint64_t flags);
 
 #endif
--- sys/src/mm/vmm.c
+++ sys/src/mm/vmm.c
@@ -1,6 +1,47 @@
 #include <mm/vmm.h>
+#include <mm/pmm.h>
+#include <lib/asm.h>
+#include <lib/assert.h>
+
 
 volatile struct limine_hhdm_request hhdm_request = {
   .id = LIMINE_HHDM_REQUEST,
   .revision = 0
 };
+
+
+static inline void __tlb_flush_single(vaddr_t ptr) {
+  ASMV("invlpg (%0)" :: "r" (ptr) : "memory");
+}
+
+
+static uintptr_t* get_next_level(pagemap_t* pagemap, uint16_t index, uint8_t do_alloc) {
+  if (pagemap[index] & PTE_PRESENT) {
+    return ((uintptr_t*)pagemap[index]);
+  }
+
+  if (!(do_alloc)) {
+    return NULL;
+  }
+
+  uintptr_t next_level = pmm_alloc();
+  ASSERT(next_level != 0, "Failed to allocate frame!\n");
+
+  pagemap[index] = (pagemap_t)next_level | PTE_PRESENT | PTE_WRITABLE;
+  return (uintptr_t*)next_level;
+}
+
+
+void vmm_map_page(pagemap_t top_level, vaddr_t vaddr, paddr_t paddr, uint64_t flags) {
+  size_t pml4_index = (vaddr & ((size_t)0x1FF << 39)) >> 39;
+  size_t pdpt_index = (vaddr & ((size_t)0x1FF << 30)) >> 30;
+  size_t pd_index = (vaddr & ((size_t)0x1FF << 21)) >> 21;
+  size_t pt_index = (vaddr & ((size_t)0x1FF << 12)) >> 12;
+
+ 
+  uintptr_t* pdpt = get_next_level((pagemap_t*)top_level, pml4_index, 1);
+  uintptr_t* pd = get_next_level(pdpt, pdpt_index, 1);
+  uintptr_t* pt = get_next_level(pd, pd_index, 1);
+  pt[pt_index] = paddr | flags;
+  __tlb_flush_single(vaddr);
+}
