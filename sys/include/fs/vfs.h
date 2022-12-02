ef7c16468b21f85bc1e87231a8cf73ab6f3a718d
Ian <spurious.vector@gmail.com>
2022-12-02 17:43:25 -0500

    VFS: Add path parsing.

--- sys/include/fs/vfs.h
+++ sys/include/fs/vfs.h
@@ -6,16 +6,15 @@
 #include <lib/hashmap.h>
 
 
-
 struct vfs_fs;
-
 
 typedef struct vfs_node {
   struct vfs_node* mountpoint;
   struct vfs_node* redir;
   struct vfs_fs* filesystem;
-  char* name;
+  char* path;
   struct vfs_node* parent;
+  size_t n_children;
   HASHMAP_TYPE(struct vfs_node*) children;
 } vfs_node_t;
 
@@ -25,7 +24,7 @@ typedef struct vfs_fs {
 } vfs_fs_t;
 
 
-vfs_node_t* vfs_make_node(vfs_fs_t* fs, vfs_node_t* parent, const char* name, uint8_t is_dir);
+vfs_node_t* vfs_make_node(vfs_fs_t* fs, vfs_node_t* parent, const char* path, uint8_t is_dir);
 void vfs_init(void);
 
 
--- sys/src/fs/vfs.c
+++ sys/src/fs/vfs.c
@@ -3,29 +3,113 @@
 #include <mm/heap.h>
 #include <lib/string.h>
 #include <lib/log.h>
+#include <lib/asm.h>
 
-static spinlock_t lock = 0;
+_unused static spinlock_t lock = 0;
 static vfs_node_t* vfs_root = NULL;
 static HASHMAP_TYPE(vfs_fs_t*) filesystems;
 
 
-vfs_node_t* vfs_make_node(vfs_fs_t* fs, vfs_node_t* parent, const char* name, uint8_t is_dir) {
+_unused static uint8_t is_path_valid(const char* path) {
+  for (const char* ptr = path; *ptr; ++ptr) {
+    switch (*ptr) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '-':
+      case '_':
+        break;
+      default:
+        if (*ptr >= 'A' && *ptr <= 'Z') break;
+        if (*ptr >= 'a' && *ptr <= 'z') break;
+        return 0;
+    }
+  }
+
+  return 1;
+}
+
+
+static inline vfs_node_t* get_node(vfs_node_t* parent, const char* name) {
+  vfs_node_t* node;
+
+  if (parent->n_children == 0) {
+    return NULL;
+  }
+
+  if (!(HASHMAP_GET(&parent->children, node, name, kstrlen(name)))) return NULL;
+  return node;
+}
+
+
+_unused static vfs_node_t* path_to_node(const char* path) {
+  if (*path == '/') ++path;
+
+  vfs_node_t* last_parent = vfs_root;
+  size_t bufidx = 0;
+  char* buf = kmalloc(kstrlen(path) + 1);
+  buf[kstrlen(path)] = 0;
+  
+  const char* ptr = path;
+  
+  while (1) {
+    if (*ptr == '\0' || (*ptr == '/' && *(ptr + 1) != '\0')) {
+      printk("%s\n", buf);
+      buf[bufidx++] = 0;
+      last_parent = get_node(last_parent, buf);
+      if (last_parent == NULL) {
+        kfree(buf);
+        return NULL;
+      }
+
+      if (*ptr == '\0') {
+        kfree(buf);
+        break;
+      }
+
+      bufidx = 0;
+      ++ptr;
+    }
+
+    buf[bufidx++] = *ptr;
+    ++ptr;
+  }
+  
+
+  return last_parent != vfs_root ? last_parent : NULL;
+}
+
+
+vfs_node_t* vfs_make_node(vfs_fs_t* fs, vfs_node_t* parent, const char* path, uint8_t is_dir) {
   vfs_node_t* node = kmalloc(sizeof(vfs_node_t));
-  node->name = kmalloc(kstrlen(name) + 1);
-  kmemcpy(node->name, name, kstrlen(name));
+  node->path = kmalloc(kstrlen(path) + 1);
+  kmemcpy(node->path, path, kstrlen(path));
 
   node->parent = parent;
   node->filesystem = fs;
 
   if (is_dir) {
     node->children = (typeof(node->children))HASHMAP_INIT(256);
+    node->n_children = 0;
+  } 
+
+  if (parent != NULL) {
+    HASHMAP_INSERT(&parent->children, path, kstrlen(path), node);
+    ++parent->n_children;
   }
 
   return node;
 }
 
 void vfs_init(void) {
-  vfs_root = vfs_make_node(NULL, NULL, "", 0);
+  vfs_root = vfs_make_node(NULL, NULL, "", 1);
   filesystems = (typeof(filesystems))HASHMAP_INIT(256);
   printk("[vfs]: Finished setting up.\n");
 }
